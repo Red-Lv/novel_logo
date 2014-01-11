82ebe567d83f4524118ce782a9ce5dcc76c2f82f
lvleibing01 <lvleibing01@baidu.com>
2014-01-11 17:58:46 +0800

    adding image dir

--- lib/util.py
+++ lib/util.py
@@ -23,11 +23,16 @@ def fetch_object_key(url):
     """
 
     m = hashlib.md5()
+    key = ''
 
-    rindex = url.rfind('.')
-    suffix = url[rindex: ][1: ]
+    offset = url.rfind('.')
+    if offset == -1:
+        return key
 
-    m.update(url)
+    prefix = url[: offset]
+    suffix = url[offset + 1: ]
+
+    m.update(prefix)
     object_prefix = m.hexdigest()
     object_suffix = suffix
 
@@ -38,5 +43,6 @@ def fetch_object_key(url):
 if __name__ == '__main__':
 
     url = 'http://image.cmfu.com/books/2750457/2750457.jpg'
+    url = 'xxxxxxx'
 
     print fetch_object_key(url)
